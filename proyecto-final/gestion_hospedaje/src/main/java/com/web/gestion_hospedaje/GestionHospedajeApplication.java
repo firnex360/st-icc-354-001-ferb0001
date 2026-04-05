cc2e075635d4d102e4b88cd66959df353caf4b76
firnex <fernandorodriguezb27@gmail.com>
2026-04-05 16:46:25 -0400

    uploading creation of final project

---
+++ proyecto-final/gestion_hospedaje/src/main/java/com/web/gestion_hospedaje/GestionHospedajeApplication.java
@@ -0,0 +1,13 @@
+package com.web.gestion_hospedaje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionHospedajeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionHospedajeApplication.class, args);
+	}
+
+}
---
+++ proyecto-final/gestion_hospedaje/src/test/java/com/web/gestion_hospedaje/GestionHospedajeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.gestion_hospedaje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionHospedajeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
